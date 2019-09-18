f83197eb456922a0f5b928e9c460993339566074
Benny William Pardede <benny.william@ui.ac.id>
2019-09-18 19:46:22 +0700

    [RED] Revised unit test subject to spec

--- lists/tests.py
+++ lists/tests.py
@@ -11,9 +11,17 @@ class HomePageTest(TestCase):
 		self.assertEqual(found.func, home_page)
 
 	def test_home_page_returns_correct_html(self):
+
+		name = "Benny William Pardede"
+		npm = "1606917550"
+		role = "trequartista"
+
 		request = HttpRequest()
 		response = home_page(request)
 		html = response.content.decode('utf8')
 		self.assertTrue(html.startswith('<html>'))
-		self.assertIn('<title>To-Do lists</title>', html)
+		self.assertIn('<title>HomePage</title>', html)
+		self.assertIn(name, html)
+		self.assertIn(npm, html)
+		self.assertIn(role, html)
 		self.assertTrue(html.endswith('</html>'))
