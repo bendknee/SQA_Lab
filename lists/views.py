1bc4ab412a1672ca804b7d71dfcd72248e5ab80a
Benny William Pardede <benny.william@ui.ac.id>
2019-09-18 19:55:07 +0700

    [GREEN] Passed unit tests

--- lists/views.py
+++ lists/views.py
@@ -2,4 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-	return HttpResponse('<html><title>To-Do lists</title></html>')
+
+	html_payload = '''<html>
+			<head>
+				<title>HomePage</title>
+			</head>
+			<body>
+
+				<h4>The name's Benny.</h4>
+				<h1>Benny William Pardede</h1>
+				<p>But they usually call me the <strong>trequartista</strong></p>
+				<p>- 1606917550</p>
+				
+			</body>
+		</html>'''
+	return HttpResponse(html_payload)
