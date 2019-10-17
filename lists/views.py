1b9a0cf262dc652d3c7081ca418644f89633f578
Benny William Pardede <benny.william@ui.ac.id>
2019-10-17 22:24:09 +0700

    [REFACTOR] Extract comment conditionals as a function

--- lists/views.py
+++ lists/views.py
@@ -6,24 +6,27 @@ from lists.models import Item
 
 def home_page(request):
     items = Item.objects.all()
+    motivation_comment = pick_comment(Item.objects.count())
+    error = None
 
     if request.method == 'POST':
         item = Item.objects.create(text=request.POST['item_text'])
         try:
             item.full_clean()
             item.save()
+            return redirect('/')
         except ValidationError:
             item.delete()
             error = "You can't have an empty list item"
-            return render(request, 'home.html', {"error": error, 'items': items})
-        return redirect('/')
 
-    if Item.objects.count() >= 5:
-        comment = "There are no such thing as too much to do for a trequartista. " \
+    return render(request, 'home.html', {"error": error, 'items': items, 'motivation_comment': motivation_comment})
+
+
+def pick_comment(count):
+    if count >= 5:
+        return "There are no such thing as too much to do for a trequartista. " \
                       "They are the attack organizer after all"
-    elif 5 > Item.objects.count() > 0:
-        comment = "A trequartista always check more things to do than losing its man marker"
+    elif 5 > count > 0:
+        return "A trequartista always check more things to do than losing its man marker"
     else:
-        comment = "If a trequartista is doing nothing on an attack, then they have failed"
-
-    return render(request, 'home.html', {'items': items, 'motivation_comment': comment})
+        return "If a trequartista is doing nothing on an attack, then they have failed"
