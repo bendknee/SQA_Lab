830dc30af6673dbd8b29cbf06dc73bd10ac4df70
Benny William Pardede <benny.william@ui.ac.id>
2019-11-21 21:16:05 +0700

    [RED] All tests in chapter 19.1 - 19.5

--- superlists/settings.py
+++ superlists/settings.py
@@ -42,9 +42,9 @@ INSTALLED_APPS = [
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
-# AUTHENTICATION_BACKENDS = [
-#     'accounts.authentication.PasswordlessAuthenticationBackend',
-# ]
+AUTHENTICATION_BACKENDS = [
+    'accounts.authentication.PasswordlessAuthenticationBackend',
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
