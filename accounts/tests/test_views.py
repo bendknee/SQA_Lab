04bf6043d5b0b216aa59a14ab10d8e16581ab53d
Benny William Pardede <benny.william@ui.ac.id>
2019-12-21 16:41:50 +0700

    [REFACTOR] Removed duplicate test case

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -14,27 +14,6 @@ class SendLoginEmailViewTest(TestCase):
         })
         self.assertRedirects(response, '/')
 
-    def test_sends_mail_to_address_from_post(self):
-        self.send_mail_called = False
-
-        def fake_send_mail(subject, body, from_email, to_list):
-            self.send_mail_called = True
-            self.subject = subject
-            self.body = body
-            self.from_email = from_email
-            self.to_list = to_list
-
-        accounts.views.send_mail = fake_send_mail
-
-        self.client.post('/accounts/send_login_email', data={
-            'email': 'benny.william@example.com'
-        })
-
-        self.assertTrue(self.send_mail_called)
-        self.assertEqual(self.subject, 'Your login link for To-Do Lists')
-        self.assertEqual(self.from_email, 'noreply@superlists')
-        self.assertEqual(self.to_list, ['benny.william@example.com'])
-
     @patch('accounts.views.send_mail')
     def test_sends_mail_to_address_from_post(self, mock_send_mail):
         self.client.post('/accounts/send_login_email', data={
