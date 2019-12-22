43f97e3bc0cdc75f5458575e8241acee6c640520
Benny William Pardede <benny.william@ui.ac.id>
2019-12-23 00:58:38 +0700

    [RED] Unit test for creation_date column at List model

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
+from django.utils.datetime_safe import datetime
 
 from lists.models import Item
 
@@ -8,10 +9,12 @@ class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
         first_item = Item()
+        first_epoch = datetime.now()
         first_item.text = 'The first (ever) list item'
         first_item.save()
 
         second_item = Item()
+        second_epoch = datetime.now()
         second_item.text = 'Item the second'
         second_item.save()
 
@@ -20,8 +23,12 @@ class ItemModelTest(TestCase):
 
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
+
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertAlmostEqual(first_saved_item.creation_time.timestamp(), first_epoch.timestamp(), places=3)
+
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertAlmostEqual(second_saved_item.creation_time.timestamp(), second_epoch.timestamp(), places=3)
 
     def test_cannot_save_empty_list_items(self):
         item = Item(text='')
