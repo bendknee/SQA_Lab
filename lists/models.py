eb5ef3d67b434776f00434679009809a9314544f
Benny William Pardede <benny.william@ui.ac.id>
2019-12-23 01:09:56 +0700

    [GREEN] implemented alter table (new column)

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Item(models.Model):
     text = models.TextField(default='')
+    creation_time = models.DateTimeField(default=timezone.now)
